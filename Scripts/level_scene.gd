a09148b0ad322efd6f45abe2304fb09670cf77ad
Paul1379 <raphael.vasse@outlook.com>
2026-07-04 12:26:45 +0200

    Change UI and anchors to 1080p screen size

--- Scripts/level_scene.gd
+++ Scripts/level_scene.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var minigames_scenes : Array[PackedScene]
-
+@export var minigames_done_text_node : RichTextLabel
+@export var current_score_text_node : RichTextLabel
 func _ready() -> void:
-	$"Minigames done number".text = "Minigames done: " + str(Global.minigames_done) +"/" + str(minigames_scenes.size())
-	$"Current score".text = "Current score: " + str(Global.score)
+	minigames_done_text_node.text = "Minigames done: " + str(Global.minigames_done) +"/" + str(minigames_scenes.size())
+	current_score_text_node.text = "Current score: " + str(Global.score)
 func _on_timer_timeout() -> void:
 	if Global.minigames_done < minigames_scenes.size():
 		get_tree().change_scene_to_packed(minigames_scenes[Global.minigames_done])
